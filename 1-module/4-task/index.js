0918b5b0f856a41a2b294559fec85704b0aa4e53
elenalshv <e.v.loshkareva@gmail.com>
2020-06-23 18:46:53 +0300

    решение Занятие 1: Основы JavaScript (update 2)

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -5,11 +5,10 @@
  */
  function checkSpam(str) {
    let arr=['1xBet','XXX'];
-   let result;
+   let result = false;
    for ( let i=0; arr.length>i; i++) {
-      result= (str.toLowerCase().includes(arr[i].toLowerCase()));
-   if (result==true) break;
-   }
+      if (result!=true) result= (str.toLowerCase().includes(arr[i].toLowerCase()));
+      }
   return result;
 
  }
