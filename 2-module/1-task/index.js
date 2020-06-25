292612760866c0d1650473af48833cbaef73487a
elenalshv <e.v.loshkareva@gmail.com>
2020-06-25 10:32:51 +0300

    занятие 1 5 задач, занятие 2 задача 1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,6 +3,23 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
+ let salary= {
+ John: 1000,
+ Ann: 1600,
+ Pete: 1300,
+ month: 'December',
+ currency: 'USD',
+ isPayed: false
+}
+
 function sumSalary(salaries) {
-  // ваш код...
+let salariessumm = 0;
+for (let key in salaries)
+{
+if ((typeof salaries[key]) === 'number' && salaries[key] !== NaN)
+{
+  salariessumm=salariessumm+salaries[key];
+}
+}
+return salariessumm;
 }
