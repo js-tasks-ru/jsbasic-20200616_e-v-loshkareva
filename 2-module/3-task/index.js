67ebdd26e99903a26609d3ad76b379d658d9ebe1
elenalshv <e.v.loshkareva@gmail.com>
2020-06-26 13:35:28 +0300

    занятие 2 задача 3

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,23 @@
 let calculator = {
-  // ваш код
-};
+  number1 : 0,
+  number2 : 0,
+}
+calculator.read= function(a,b)
+{
+   this.number1=a;
+   this.number2=b;
+
+}
+calculator.sum= function()
+{
+  let sum=this.number1+this.number2;
+  return sum;
+}
+calculator.mul=function()
+{
+  let mul=this.number1*this.number2;
+  return mul;
+}
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
