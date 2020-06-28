8c194ba3bddf1670648fde130eb737322a10499a
elenalshv <e.v.loshkareva@gmail.com>
2020-06-28 23:27:28 +0300

    занятие 3 задача 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -2,6 +2,13 @@
  * @param {string} str
  * @returns {string}
  */
-function camelize(str) {
-  // ваш код...
-}
+ function camelize(str) {
+  let result=str.split('-');
+  for (let i in result){
+           if (i!=0) {
+      result[i]=result[i][0].toUpperCase()+result[i].slice(1);
+    }
+     }
+  result=result.join('');
+  return result;
+ }
