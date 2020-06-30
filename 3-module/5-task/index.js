2641d0839a8907deaf1e7d6ac8223be3a2340d94
elenalshv <e.v.loshkareva@gmail.com>
2020-06-30 18:24:18 +0300

    занятие 3 задача 5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -3,6 +3,33 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
+// Add your code here
+
+
 function getMinMax(str) {
-  // ваш код...
+  let result = str;
+  let i = 0;
+  while (i !== -1) {
+    i = result.indexOf(',');
+    if (i !== -1) result = result.slice(0, i) + ' ' + result.slice(i + 1, result.length);
+  }
+  result = result.split(' ');
+  //console.log(result);
+  let min, max;
+  result.forEach(function(val) {
+
+    if (!isNaN(Number(val)) && val != '') {
+      if (min == undefined) min = Number(val);
+      if (max == undefined) max = Number(val);
+      min = Math.min(min, Number(val));
+      max = Math.max(max, Number(val));
+    }
+  });
+
+  result = {
+    min: min,
+    max: max,
+  };
+  return result;
 }
+console.log(getMinMax(inputData)); // { min: -5.8, max: 73  }
