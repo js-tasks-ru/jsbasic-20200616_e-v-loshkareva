7d046c3013fd50cc4a6f98daf95acb4c6b766a85
elenalshv <e.v.loshkareva@gmail.com>
2020-07-14 18:16:28 +0300

    занятие 6 задача 2

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -33,9 +33,7 @@
 export default class UserTable {
   constructor(rows) {
     this.rowNum = rows;
-  //  get elem() {
-      let clickEvent = new MouseEvent('click', { bubbles: true });
-
+  //  get elem()
       let tbl = document.createElement("table");
       let thead = document.createElement("thead");
       let tblBody = document.createElement("tbody");
@@ -48,23 +46,7 @@ export default class UserTable {
         let button = document.createElement("button");
         button.onclick =  () => {
           row1.remove();
-          //var element = document.getElementById("top");
-  //while (row1.firstChild) {
-  //  row1.removeChild(row1.firstChild);
-  //}
-  //row1.remove();
-  //tblBody.removeChild(row1);
-  //row1 = null;
-  //console.log(row1);
-  //console.log(tbl.querySelector('tbody tr'));
-  }
-  //row1.remove(); row1 = null;};
-      //  button.onclick = function() {
-
-          //let removeRow= document.getElementById('idStr');
-          //removeRow.parentNode.removeChild(removeRow);
-              //  };
-
+      }
         row1.innerHTML = `<td>${this.rowNum[i].name}</td><td>${this.rowNum[i].age}</td><td>${this.rowNum[i].salary}</td><td>${this.rowNum[i].city}</td>`;
         let td = document.createElement("td");
         button.innerHTML = "X";
@@ -74,11 +56,7 @@ export default class UserTable {
       }
       tbl.appendChild(tblBody);
 
-      //return 123;
-      //  this.table.appendChild('111');
-      //  console.log(this.rowNum);
 
-    //  return tbl;
    this.elem = tbl;
 
   }
