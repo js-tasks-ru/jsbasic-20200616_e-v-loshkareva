43732983a998caeca321d935bcf23c8485942205
elenalshv <e.v.loshkareva@gmail.com>
2020-08-13 22:45:10 +0300

    9.1

--- 9-module/1-task/index.js
+++ 9-module/1-task/index.js
@@ -5,7 +5,11 @@
  */
 
 export default function promiseClick(button) {
-  return  new Promise(function(resolve, reject){
-   button.addEventListener('click', (event) => {
-  resolve(value);   }, { once: true });
-  }
+  return new Promise(function(resolve, reject) {
+        button.addEventListener('click', (event) => {
+          resolve(event);
+        }, {
+          once: true
+        });
+      })
+          }
